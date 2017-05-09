d45d0beb0dee6a094e845369de5e178b52038fe2
孟庆祝 <mqzadyq@bupt.edu.cn>
2017-05-09 19:11:01 +0800

    resolve proxy at 5.9, see detail at README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# interview
+## interview
 resolve problems of interview 360
+
+### proxy
+ - 1.无内存泄漏，无未关闭句柄
+ - 2.时间复杂度O(2n),空间复杂度O(n)
---
+++ proxy_url/proxy_url/proxy_url_extractor.cc
@@ -0,0 +1,143 @@
+#include <iostream>
+#include "proxy_url_extractor.h"
+#include <fstream>
+#include <vector>
+#include "tokener.h"
+
+namespace qh
+{
+
+    namespace {
+
+        template< class _StringVector, 
+        class StringType,
+        class _DelimType> 
+            inline void StringSplit(  
+            const StringType& str, 
+            const _DelimType& delims, 
+            unsigned int maxSplits, 
+            _StringVector& ret)
+        {
+            unsigned int numSplits = 0;
+
+            // Use STL methods
+            size_t start, pos;
+            start = 0;
+
+            do
+            {
+                pos = str.find_first_of( delims, start );
+
+                if ( pos == start )
+                {
+                    ret.push_back(StringType());
+                    start = pos + 1;
+                }
+                else if ( pos == StringType::npos || ( maxSplits && numSplits + 1== maxSplits ) )
+                {
+                    // Copy the rest of the string
+                    ret.push_back(StringType());
+                    *(ret.rbegin()) = StringType(str.data() + start, str.size() - start);
+                    break;
+                }
+                else
+                {
+                    // Copy up to delimiter
+                    //ret.push_back( str.substr( start, pos - start ) );
+                    ret.push_back(StringType());
+                    *(ret.rbegin()) = StringType(str.data() + start, pos - start);
+                    start = pos + 1;
+                }
+
+                ++numSplits;
+
+            }
+            while ( pos != StringType::npos );
+        }
+    }
+
+    ProxyURLExtractor::ProxyURLExtractor()
+    {
+    }
+
+    bool ProxyURLExtractor::Initialize( const std::string& param_keys_path )
+    {
+        std::ifstream ifs;
+        ifs.open(param_keys_path.data(), std::fstream::in);
+        typedef std::vector<std::string> stringvector;
+        stringvector keysvect;
+        
+        while (!ifs.eof()) {
+            std::string line;
+            getline(ifs, line);
+            if (ifs.fail() && !ifs.eof()) {
+                fprintf(stderr, "SubUrlExtractor::LoadParamKeysFile readfile_error=[%s] error!!", param_keys_path.data());
+                ifs.close();
+                return false;
+            }
+            if (line.empty()) continue;
+
+            keysvect.clear();
+            StringSplit(line, ",", static_cast<unsigned int>(-1), keysvect);
+            assert(keysvect.size() >= 1);
+            keys_set_.insert(keysvect.begin(), keysvect.end());
+            keys_set_.erase("");
+        }
+
+        ifs.close();
+
+        return true;
+    }
+
+    std::string ProxyURLExtractor::Extract( const std::string& raw_url )
+    {
+        std::string sub_url;
+        ProxyURLExtractor::Extract(keys_set_, raw_url, sub_url);
+        return sub_url;
+    }
+
+    void ProxyURLExtractor::Extract( const KeyItems& keys, const std::string& raw_url, std::string& sub_url )
+    {
+		sub_url = std::string();
+        Tokener token(raw_url);
+        token.skipTo('?');
+		while(token.current() == '?') token.next();
+		while(!token.isEnd()){
+			const char * pStart = token.getCurReadPos();
+			while(token.current() == '&') token.next();
+			std::string cur = token.nextString('&');
+			if(cur.empty()){
+				std::string last(pStart, token.getReadableSize());
+				Tokener temp(last);
+				cur = temp.nextString('=');
+				if(cur.empty()){
+					if(keys.find(last) != keys.end()) return;
+				}else{
+					if(keys.find(cur) != keys.end()){ 
+						sub_url = std::string(temp.getCurReadPos(), temp.getReadableSize());
+					}
+				}
+				return;
+			}else{
+				Tokener temp(cur);
+				std::string key = temp.nextString('=');
+				if(key.empty()){
+					if(keys.find(cur) != keys.end()) return;
+				}else{
+					if(keys.find(key) != keys.end()){
+					   	sub_url = std::string(temp.getCurReadPos(), temp.getReadableSize());
+						return;
+					}
+				}
+			}
+		}
+    }
+
+    std::string ProxyURLExtractor::Extract( const KeyItems& keys, const std::string& raw_url )
+    {
+        std::string sub_url;
+        ProxyURLExtractor::Extract(keys, raw_url, sub_url);
+        return sub_url;
+    }
+}
+
